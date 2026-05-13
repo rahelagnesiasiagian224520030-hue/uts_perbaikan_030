01130f897451437ca4eb6afc98910ba0f93187ff
rahelagnesiasiagian224520030-hue <rahelagnesiasiagian224520030@gmail.com>
2026-05-13 17:02:34 +0700

    membuat lamodel kendaraan  dan proteksi mass assigment

---
+++ app/Models/Kendaraan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kendaraan extends Model
+{
+
+
+    protected $fillable = ['plat_nomor', 'nama_pemilik', 'merk_kendaraan', 'keluhan'];
+}  //
+
